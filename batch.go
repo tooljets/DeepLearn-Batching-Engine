6ec075fc32a7f4d73a964bd173594ff2f136dc6c
tooljets <130756254+tooljets@users.noreply.github.com>
2023-05-26 21:25:16 +0000

    method rename

--- batch.go
+++ batch.go
@@ -106,3 +106,72 @@ func NewBatching(address, protocol string, batchSize, capacity int, maxLatency, 
 func (b *Batching) HandleHTTP(ctx *fasthttp.RequestCtx) {
 	data := ctx.PostBody()
 	if len(data) == 0 {
+		// as a naive health check
+		ctx.SetStatusCode(200)
+		b.logger.Info("Health check request")
+		return
+	}
+
+	job := newJob(data, b.timeout)
+	// append job to the queue
+	b.logger.Info("Add job to the queue", zap.String("jobID", job.id))
+	select {
+	// append to the queue
+	case b.queue <- job:
+		b.jobsLock.Lock()
+		b.jobs[job.id] = job
+		b.jobsLock.Unlock()
+	default:
+		// queue is full
+		b.logger.Warn("Queue is full. Return error code 429")
+		ctx.SetStatusCode(429)
+		job.done <- true
+	}
+
+	// waiting for job done by workers
+	select {
+	case <-job.done:
+		ctx.SetStatusCode(job.statusCode)
+		ctx.SetBody(job.data)
+	case <-time.After(b.timeout):
+		//  timeout
+		b.jobsLock.Lock()
+		delete(b.jobs, job.id)
+		b.jobsLock.Unlock()
+		b.logger.Warn("Delete timeout job", zap.String("jobID", job.id))
+		ctx.TimeoutError("Timeout!")
+	}
+}
+
+// Stop the batching service and socket, close the queue channel
+func (b *Batching) Stop() error {
+	b.logger.Info("Trying to terminate the batch service")
+	time.Sleep(b.timeout)
+	b.logger.Info("Close socket and queue channel, flush logging")
+	defer b.logger.Sync()
+	close(b.queue)
+	return b.socket.Close()
+}
+
+// send a batch jobs to the workers
+func (b *Batching) send(conn net.Conn) error {
+	batch := make(String2Bytes)
+	job, ok := <-b.queue
+	if !ok {
+		return fmt.Errorf("queue is closed")
+	}
+	batch[job.id] = job.data
+	// timing from getting the first query data
+	waitUntil := time.Now().Add(b.maxLatency)
+	// check latency and batch size
+	for time.Now().Before(waitUntil) && len(batch) < b.batchSize {
+		select {
+		case job := <-b.queue:
+			// expired job
+			if time.Now().After(job.expire) {
+				b.logger.Info("Job already expired before sent to the worker", zap.String("jobID", job.id))
+				job.statusCode = 408
+				job.done <- true
+				continue
+			}
+			// append job to the batch
