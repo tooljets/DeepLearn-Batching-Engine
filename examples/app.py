158206a694e8ea1e007ac795259ec2b36e44da2a
tooljets <130756254+tooljets@users.noreply.github.com>
2023-05-28 18:51:38 +0000

    Tiny change

--- examples/app.py
+++ examples/app.py
@@ -5,4 +5,8 @@ from pydantic import BaseModel, confloat, constr
 from ventu import Ventu
 import torch
 import numpy as np
-from transformers import DistilBertTokenizer, DistilBertForSequenceC
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+
+class Req(BaseModel):
+    # the input sentence 
