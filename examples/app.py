35362577dc9325153e4d53f3a34f591503e38cc5
tooljets <130756254+tooljets@users.noreply.github.com>
2023-06-29 05:46:06 +0000

    unfinished

--- examples/app.py
+++ examples/app.py
@@ -77,4 +77,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Ventu service')
     parser.add_argument('--mode', '-m', default='http', choices=('http', 'unix', 'tcp'))
     parser.add_argument('--host', default='localhost')
-    p
+    parser.add_argument('--port', '-p', default=8080,
